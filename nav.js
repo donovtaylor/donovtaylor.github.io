c91b54314f70ae3ed3f82c5cda27c3fa63782701
donny <donovan.velasquez@yahoo.com>
2025-06-14 18:56:22 -0700

    added highlighting of active tab in navbar

--- nav.js
+++ nav.js
@@ -1,6 +1,24 @@
+function highlightActiveTab() {
+    var top_bar = document.querySelector('.top-bar');
+    if (top_bar) {
+        var currentPath = window.location.pathname;
+        var links = top_bar.querySelectorAll('a');
+
+        links.forEach(function(link) {
+            var href = link.getAttribute('href');
+            if (href && currentPath.includes(href)) {
+                link.classList.add('active');
+            } else {
+                link.classList.remove('active');
+            }
+        });
+    }
+}
+
 fetch("navbar.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("navbar-placeholder").outerHTML = data;
+    highlightActiveTab();
   })
   .catch(error => console.error('Error loading navbar:', error));
