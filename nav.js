288be941cf21eeaaea3151bb54f03db3a86a584a
donny <donovan.velasquez@yahoo.com>
2025-06-14 18:05:58 -0700

    added a project to the project page and added about and contact page also

--- nav.js
+++ nav.js
@@ -1,3 +1,6 @@
-$.get("navbar.html", function(data) {
-  $("#replace-with-navbar").replaceWith(data);
-});
+fetch("navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar-placeholder").outerHTML = data;
+  })
+  .catch(error => console.error('Error loading navbar:', error));
