489eeffb7c3d04dc13d4cde1d79b0ca447a116ec
donny <donovan.velasquez@yahoo.com>
2025-06-15 23:50:38 -0700

    added highlighting of first tab on first loading

--- nav.js
+++ nav.js
@@ -1,3 +1,4 @@
+// highlight the active tab in the top bar based on the current URL
 function highlightActiveTab() {
     var top_bar = document.querySelector('.top-bar');
     if (top_bar) {
@@ -12,9 +13,14 @@ function highlightActiveTab() {
                 link.classList.remove('active');
             }
         });
+        // If no link is active, set the first link as active
+        if (!top_bar.querySelector('.active') && links.length > 0) {
+            links[0].classList.add('active');
+        }
     }
 }
 
+// dynamically load the navbar
 fetch("navbar.html")
   .then(response => response.text())
   .then(data => {
